2fd6bf068354eaf2a70fcc00c99b3ae73eee1c69
BSuria <Basuria@hotmail.com>
2025-02-24 07:05:52 -0500

    Improved Chat Sidebare Layout, added Chat Sheet, Drafted Message receiving

--- src/app/chat/chat-layout.tsx
+++ src/app/chat/chat-layout.tsx
@@ -1,11 +1,116 @@
-import { SidebarProvider } from '@/components/ui/sidebar';
+'use client';
+import { useState } from 'react';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { ChatSidebar } from '@/components/chat-sidebar';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+import { AlertTriangle, FileClock, MapPin, Menu, House } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export default function Chat() {
+  interface item {
+    id: string;
+    title: string;
+    messages: string[];
+    url: string;
+    icon: string; // Array of messages
+  }
+
+  const [selectedChat, setSelectedChat] = useState<item | null>(null);
   return (
     <>
       <SidebarProvider>
-        <ChatSidebar />
+        <ChatSidebar setSelectedChat={setSelectedChat} />
+        <SidebarInset>
+          <header className="bg-accent fixed top-0 flex w-317 items-center justify-between p-4">
+            <div className="flex items-center gap-4">
+              <Avatar className="size-10 md:block">
+                <AvatarImage />
+                <AvatarFallback className="bg-card">CN</AvatarFallback>
+              </Avatar>
+              <span>
+                {selectedChat
+                  ? selectedChat.title
+                  : 'Should autoselect the last person messaged probably'}
+              </span>
+            </div>
+            <div className="x flex items-center gap-4">
+              <nav>
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button variant={'ghost'} size={'icon'}>
+                      <Menu className="size-auto" />
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent>
+                    <SheetHeader>
+                      <SheetTitle className="flex flex-col items-center justify-center gap-4">
+                        <span>
+                          {selectedChat ? selectedChat.title : 'Pick Somebody'}
+                        </span>
+                        <Avatar className="size-28 md:block">
+                          <AvatarImage src="/logo.svg" />
+                          <AvatarFallback className="bg-accent">
+                            CN
+                          </AvatarFallback>
+                        </Avatar>
+                      </SheetTitle>
+                      <SheetDescription className="mt-10">
+                        <span>Description and Some Buttons</span>
+                        <ul className="absolute bottom-20 ml-5 space-y-4 text-2xl">
+                          <li className="flex cursor-pointer items-center gap-2 hover:underline">
+                            <House />
+                            <span className="ml-2">Home</span>
+                          </li>
+                          <li className="flex cursor-pointer items-center gap-2 hover:underline">
+                            <FileClock />
+                            <span className="ml-2">Time Table</span>
+                          </li>
+                          <li className="flex cursor-pointer items-center gap-2 hover:underline">
+                            <AlertTriangle />
+                            <span className="ml-2">Alerts</span>
+                          </li>
+                        </ul>
+                        <span className="absolute bottom-0 mb-5 flex">
+                          <MapPin />
+                          <span className="ml-2">
+                            [Current Vehicle or Stop]
+                          </span>
+                        </span>
+                      </SheetDescription>
+                    </SheetHeader>
+                  </SheetContent>
+                </Sheet>
+              </nav>
+            </div>
+          </header>
+          <div className="flex-1 space-y-12 p-4">
+            {selectedChat ? (
+              <>
+                <h3 className="text-xl">{selectedChat.title}</h3>
+                <div className="mt-4">
+                  {selectedChat.messages.map((message, index) => (
+                    <div
+                      key={index}
+                      className="bg-accent mb-5 max-w-[30%] rounded-md p-2 break-words"
+                    >
+                      <p>{message}</p>
+                    </div>
+                  ))}
+                </div>
+              </>
+            ) : (
+              <p></p>
+            )}
+          </div>
+        </SidebarInset>
       </SidebarProvider>
     </>
   );
--- src/components/chat-sidebar.tsx
+++ src/components/chat-sidebar.tsx
@@ -1,102 +1,119 @@
-'use client';
 import { useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarSeparator,
-  SidebarHeader,
 } from '@/components/ui/sidebar';
 
-// Menu items.
-const items = [
+interface item {
+  id: string;
+  title: string;
+  messages: string[];
+  url: string;
+  icon: string; // Array of messages
+}
+
+const items: item[] = [
   {
+    id: '1',
     title: 'Person',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PE',
   },
   {
+    id: '2',
     title: 'Person1',
-    content: 'Hello',
+    messages: [
+      'Helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo',
+      'Bye',
+    ],
     url: '#',
     icon: 'PR',
   },
   {
+    id: '3',
     title: 'Person2',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PS',
   },
   {
+    id: '4',
     title: 'Person3',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PO',
   },
   {
+    id: '5',
     title: 'Person4',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PN',
   },
   {
+    id: '6',
     title: 'Person5',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PN',
   },
   {
+    id: '7',
     title: 'Person6',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PN',
   },
   {
+    id: '8',
     title: 'Person7',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PN',
   },
   {
+    id: '9',
     title: 'Person8',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PN',
   },
   {
+    id: '10',
     title: 'Person9',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PN',
   },
   {
+    id: '11',
     title: 'Person10',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PN',
   },
   {
+    id: '12',
     title: 'Person11',
-    content: 'Hello',
-    url: '#',
-    icon: 'PN',
-  },
-  {
-    title: 'Person12',
-    content: 'Hello',
+    messages: ['Hello', 'Bye'],
     url: '#',
     icon: 'PN',
   },
 ];
 
-export function ChatSidebar() {
+export function ChatSidebar({
+  setSelectedChat,
+}: {
+  setSelectedChat: (item: item) => void;
+}) {
   const [searchQuery, setSearchQuery] = useState('');
 
   const filteredItems = items.filter((item) =>
@@ -104,57 +121,67 @@ export function ChatSidebar() {
   );
 
   return (
-    <Sidebar className="bg-accent w-84">
+    <Sidebar className="bg-sidebar-accent">
+      <SidebarHeader className="items-center">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex w-14 justify-between space-x-10">
+                  <Avatar className="size-12 md:block">
+                    <AvatarImage />
+                    <AvatarFallback className="bg-accent">ME</AvatarFallback>
+                  </Avatar>
+                </div>
+                <span className="">User</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="bg-background text-foreground w-full rounded-lg p-2 focus:outline-none"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)} // Update search query
+        />
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarHeader className="items-center">
-              <SidebarGroupLabel className="flex w-20 justify-between space-x-10">
-                <Avatar>
-                  <AvatarImage src="/logo.svg" />
-                  <AvatarFallback className="bg-card">CN</AvatarFallback>
-                </Avatar>
-                User
-              </SidebarGroupLabel>
-              <input
-                type="text"
-                placeholder="Search..."
-                className="bg-background text-foreground w-full rounded-lg p-2 focus:outline-none"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)} // Update search query
-              />
-            </SidebarHeader>
-            <SidebarMenu>
-              {filteredItems.map((item) => (
-                <SidebarMenuItem key={item.title} className="h-18 min-h-16">
-                  <SidebarSeparator className="bg-foreground w-" />
-                  <SidebarMenuButton asChild className="h-18 min-h-16">
-                    <a href={item.url}>
-                      <div className="flex w-80 items-center justify-between space-x-4">
-                        <Avatar>
-                          <AvatarImage />
-                          <AvatarFallback className="bg-card">
-                            {item.icon}
-                          </AvatarFallback>
-                        </Avatar>
-                        <div className="flex-1 space-y-1">
-                          <div className="flex w-full justify-between">
-                            <h4 className="space-y-1 text-sm font-semibold">
-                              {item.title}
-                            </h4>
-                            <span className="text-muted-foreground space-y-1 text-xs">
-                              31 Minutes Ago
-                            </span>
-                          </div>
-                          <p className="text-sm">{item.content}</p>
+          <SidebarMenu>
+            {filteredItems.map((item) => (
+              <SidebarMenuItem key={item.title} className="h-18 min-h-16">
+                <SidebarSeparator className="bg-foreground w-" />
+                <SidebarMenuButton
+                  asChild
+                  className="h-18 min-h-16"
+                  onClick={() => setSelectedChat(item)}
+                >
+                  <a href={item.url}>
+                    <div className="flex w-80 items-center justify-between space-x-4">
+                      <Avatar className="size-12 md:block">
+                        <AvatarImage />
+                        <AvatarFallback className="bg-card">
+                          {item.icon}
+                        </AvatarFallback>
+                      </Avatar>
+                      <div className="flex-1 space-y-1">
+                        <div className="flex w-full justify-between">
+                          <h4 className="space-y-1 text-sm font-semibold">
+                            {item.title}
+                          </h4>
+                          <span className="text-muted-foreground space-y-1 text-xs">
+                            31 Minutes Ago
+                          </span>
                         </div>
+                        <p className="text-sm">{item.messages.at(-1)}</p>
                       </div>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
+                    </div>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
