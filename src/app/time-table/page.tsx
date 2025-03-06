a586819fe17766930abb21c6091b5b3c7e47e1b5
Daniel Stoiber <dastoiboy@gmail.com>
2025-03-06 11:19:52 -0500

    Adding basic metadata to ssr pages

--- src/app/time-table/page.tsx
+++ src/app/time-table/page.tsx
@@ -1,5 +1,17 @@
 import { TrainSchedule } from '@/components/TrainSchedule';
 import { Header } from '@/components/Header';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Huddle Chat - Time Table',
+  description: 'Check the MBTA train schedule through Huddle Chat!',
+  openGraph: {
+    type: 'website',
+    url: process.env.VERCEL_PROJECT_PRODUCTION_URL,
+    title: 'Huddle Chat - Time Table',
+    description: 'Check the MBTA train schedule through Huddle Chat!',
+  },
+};
 
 export default function TimeTablePage() {
   return (
